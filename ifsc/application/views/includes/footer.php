b08ba5864aa3bc44a9f73a26330ba8eab15c3316
venkatesh <damovenkatesh@gmail.com>
2016-02-19 19:28:14 +0530

    file modified

--- ifsc/application/views/includes/footer.php
+++ ifsc/application/views/includes/footer.php
@@ -2,7 +2,7 @@
 	<div class="footer-inner">
 		<div class="container">
 			<div class="row">
-				<div class="span12"> &copy; <?php echo date('Y');?> <?php echo anchor('http://www.dialbe.com', 'Dialbe.com',array('target'=>'blank')); ?>. </div>
+				<div class="span12"> &copy; <?php echo date('Y');?> <?php echo anchor('http://www.ifsc.com', 'Ifsc.com',array('target'=>'blank')); ?>. </div>
 			<!-- /span12 --> 
 			</div>
 		  <!-- /row --> 
